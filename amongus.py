06df4c87dac33d02cba818973f0d99366b83dce8
lilmayofuksu <ridvan-nuri@windowslive.com>
2022-10-29 20:47:57 +0300

    fix smol issue

--- amongus.py
+++ amongus.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     # Open the binary
     with open(binary, "rb") as f:
-        pattern = rb"\x62\x06\x70\x72\x6F\x74\x6F\x33\x00"
+        pattern = b"\x62\x06\x70\x72\x6F\x74\x6F\x33\x00"
         data = f.read()
         match: list[re.Match] = list(re.finditer(pattern, data))
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
                     startoffset -= 1
                 
                 if data[startoffset] == 0x00 and data[startoffset + 1] == 0x0a:
-                    binary_data = data[startoffset+1:endoffset]
+                    binary_data = data[startoffset+1:endoffset+len(pattern)-1]
 
                     with open(f"out/{idx}.bin", "wb") as outf:
                         outf.write(binary_data)
